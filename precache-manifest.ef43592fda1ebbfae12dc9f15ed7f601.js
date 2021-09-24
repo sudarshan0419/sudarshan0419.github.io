c7a493c2d885dd7b7830fa9ceae1df60a8e07a08
byrrice <32532726+byrrice@users.noreply.github.com>
2021-09-23 21:29:23 -0600

    Updates

---
+++ precache-manifest.ef43592fda1ebbfae12dc9f15ed7f601.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a83f16471af310ac1940cc182c88b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46118e986eddc7e2a1a4",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "572be5c9f76f6a8652c8",
+    "url": "/static/css/main.750477b1.chunk.css"
+  },
+  {
+    "revision": "46118e986eddc7e2a1a4",
+    "url": "/static/js/2.6237289e.chunk.js"
+  },
+  {
+    "revision": "572be5c9f76f6a8652c8",
+    "url": "/static/js/main.a4686c60.chunk.js"
+  },
+  {
+    "revision": "e84e8d6bce7e483cf120",
+    "url": "/static/js/runtime-main.3f435416.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "091c95a88d3499dcf904d784fc74c815",
+    "url": "/static/media/justicegrad.091c95a8.ttf"
+  }
+]);
