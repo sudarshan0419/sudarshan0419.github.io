726335302b9728ff011ee2789c6974d32db98576
byrrice <32532726+byrrice@users.noreply.github.com>
2021-09-23 21:34:25 -0600

    adding gh-pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
+import suds from "../images/suds.jpg";
 
 export default function Home() {
   return (
     <div className="suds">
       <div className="lander">
         <h1 id="title">Sudarshan Pillai</h1>
+        <img src = {suds}/>
       </div>
     </div>
   );
